202fa83d8be4ffa34c7f0b61653038ea0a14b3c0
jeewon Park <yuha8486@gmail.com>
2021-12-01 17:17:27 +0900

    서비스 먼저 작업 중

---
+++ Assets/Scripts/Server/NetworkService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkService
+{
+    //접속 확인 
+    Listener clientListener;
+
+    //소켓 메서드 호출 시 필요, 인터페이스 안생성 해도됨
+    SocketAsyncEventArgsPool receiveEventArgsPool;
+    SocketAsyncEventArgsPool sendEventArgsPool;
+
+
+    //데이터 송수신 버퍼 관리 매니저 객체
+    BufferManager bufferMgr;
+
+    //클라 접속시 통보 수단
+    public delegate void SessionHandler(UserToken token);
+    public SessionHandler sessionCallback { get; set; }
+
+
+    public void Listen(string host, int port, int backlog)
+    {
+        Listener listener = new Listener();
+        listener.OnNewClientCallback += OnNewCliConnected;
+        listener.Start(host, port, backlog);
+    }
+
+    private void OnNewCliConnected(Socket clientSoc, object token)
+    {
+        
+    }
+}
